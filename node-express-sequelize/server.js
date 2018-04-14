046483ef52ec959076f76619de9e6e6f240943e6
MohammadAlRifai <mohammad94sar@gmail.com>
2018-04-14 09:48:53 +0300

    M

--- node-express-sequelize/server.js
+++ node-express-sequelize/server.js
@@ -9,7 +9,9 @@ var User = sequelize.define('User', {
 
 /*  Create a '/users' route that responds to 
     a GET request with all users in the database */
-
+app.get('/users', function(req, res){
+  res.send('User');
+});
 
 
 module.exports = { 
